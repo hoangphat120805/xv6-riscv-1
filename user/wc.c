fa53621edab4151a6ffb258c71ef008cf206c7d1
hoangphat120805 <hoangphatnguyen1208@gmail.com>
2026-02-05 14:06:49 +0000

    fea: modified wc

--- user/wc.c
+++ user/wc.c
@@ -6,7 +6,7 @@
 char buf[512];
 
 void
-wc(int fd, char *name)
+wc(int fd, char *name, int show_lines, int show_words, int show_chars)
 {
   int i, n;
   int l, w, c, inword;
@@ -30,25 +30,53 @@ wc(int fd, char *name)
     printf("wc: read error\n");
     exit(1);
   }
-  printf("%d %d %d %s\n", l, w, c, name);
+  
+  // Print requested counts
+  if(show_lines)
+    printf("%d ", l);
+  if(show_words)
+    printf("%d ", w);
+  if(show_chars)
+    printf("%d ", c);
+  printf("%s\n", name);
 }
 
 int
 main(int argc, char *argv[])
 {
   int fd, i;
+  int show_lines = 0, show_words = 0, show_chars = 0;
+  int first_file_arg = 1;
+  
+  for(i = 1; i < argc && argv[i][0] == '-'; i++){
+    char *opt = argv[i];
+    int j;
+    for(j = 1; opt[j] != '\0'; j++){
+      if(opt[j] == 'l')
+        show_lines = 1;
+      else if(opt[j] == 'w')
+        show_words = 1;
+      else if(opt[j] == 'c')
+        show_chars = 1;
+    }
+  }
+  first_file_arg = i;
+  
+  if(!show_lines && !show_words && !show_chars){
+    show_lines = show_words = show_chars = 1;
+  }
 
-  if(argc <= 1){
-    wc(0, "");
+  if(first_file_arg >= argc){
+    wc(0, "", show_lines, show_words, show_chars);
     exit(0);
   }
 
-  for(i = 1; i < argc; i++){
+  for(i = first_file_arg; i < argc; i++){
     if((fd = open(argv[i], O_RDONLY)) < 0){
       printf("wc: cannot open %s\n", argv[i]);
       exit(1);
     }
-    wc(fd, argv[i]);
+    wc(fd, argv[i], show_lines, show_words, show_chars);
     close(fd);
   }
   exit(0);
